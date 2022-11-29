c24e8632015b218c06f26c0ef9fb758f6e69628a
sorada <52327484+sorada1111@users.noreply.github.com>
2022-11-28 23:33:11 -0500

    add update student procesure and change exception for create student procedure

--- build_tables/Procedure._create_student.sql
+++ build_tables/Procedure._create_student.sql
@@ -15,6 +15,8 @@ CREATE OR REPLACE PROCEDURE create_student_sp
   p_dob IN STUDENT.DOB%TYPE)
   IS
   p_student_ID NUMBER;
+  CHECK_CONSTRAINT_VIOLATION EXCEPTION;
+  PRAGMA EXCEPTION_INIT(CHECK_CONSTRAINT_VIOLATION, -2290);
   BEGIN
   p_student_ID := STUDENT_ID_SEQ.NEXTVAL;
   INSERT INTO STUDENT (STUDENT_ID,STUDENT_FIRST_NAME,STUDENT_MIDDLE_NAME,STUDENT_LAST_NAME,TELEPHONE,EMAIL,
@@ -22,10 +24,13 @@ CREATE OR REPLACE PROCEDURE create_student_sp
   VALUES(p_student_ID,p_student_first_name,p_student_middle_name,p_student_last_name,p_telephone,p_email,
   p_address,p_province,p_country,p_city,p_zipCode,p_isDomestic,p_identityNo,p_sin,p_dob);
   COMMIT;
-  DBMS_OUTPUT.PUT_LINE('Student ID: ' || p_student_ID || 'succesfully created to the system.');
+  DBMS_OUTPUT.PUT_LINE('Student ID: ' || p_student_ID || ' succesfully created to the system.');
   EXCEPTION
  WHEN DUP_VAL_ON_INDEX THEN 
  DBMS_OUTPUT.PUT_LINE('ERROR!!!!!!! CANNOT create new student there is a duplicate value on index');
+ ROLLBACK;
+ WHEN CHECK_CONSTRAINT_VIOLATION THEN
+ DBMS_OUTPUT.PUT_LINE('Create Student failed due to check constraint violation!!!!!');
  ROLLBACK;
  WHEN OTHERS THEN
  DBMS_OUTPUT.PUT_LINE('Error' || SQLERRM);
@@ -39,9 +44,9 @@ DECLARE
     lv_student_middle_name STUDENT.STUDENT_MIDDLE_NAME%TYPE := '';
     lv_student_last_name STUDENT.STUDENT_LAST_NAME%TYPE := 'PRATHAN';
     lv_telephone STUDENT.TELEPHONE%TYPE :='4326576492';
-    lv_email STUDENT.EMAIL%TYPE := 'ida111@gmail.com';
+    lv_email STUDENT.EMAIL%TYPE := 'id@@a111@gmail.com';
     lv_address STUDENT.ADDRESS%TYPE := '164 Madison Avenue';
-    lv_province STUDENT.PROVINCE%TYPE := 'ON';
+    lv_province STUDENT.PROVINCE%TYPE := 'AN';
     lv_country STUDENT.COUNTRY%TYPE := 'CANADA';
     lv_city STUDENT.CITY%TYPE := 'Toronto';
     lv_zipCode STUDENT.ZIP_CODE%TYPE := 'M4B 1K7';
---
+++ build_tables/Procedure_update_Student.sql
@@ -0,0 +1,43 @@
+CREATE OR REPLACE PROCEDURE update_student_sp
+( p_student_id IN STUDENT.STUDENT_ID%TYPE,
+  p_student_first_name IN STUDENT.STUDENT_FIRST_NAME%TYPE,
+  p_student_middle_name IN STUDENT.STUDENT_MIDDLE_NAME%TYPE,
+  p_student_last_name IN STUDENT.STUDENT_LAST_NAME%TYPE,
+  p_telephone IN STUDENT.TELEPHONE%TYPE,
+  p_email IN STUDENT.EMAIL%TYPE,
+  p_sin IN STUDENT.SIN%TYPE,
+  p_dob IN STUDENT.DOB%TYPE)
+  IS
+  CHECK_CONSTRAINT_VIOLATION EXCEPTION;
+  PRAGMA EXCEPTION_INIT(CHECK_CONSTRAINT_VIOLATION, -2290);
+  BEGIN
+  UPDATE STUDENT
+  SET STUDENT_FIRST_NAME = p_student_first_name, STUDENT_MIDDLE_NAME = p_student_middle_name, STUDENT_LAST_NAME = p_student_last_name, TELEPHONE = p_telephone,
+  EMAIL = p_email,SIN = p_sin, DOB = p_dob
+  WHERE STUDENT_ID = p_student_id;
+  COMMIT;
+  DBMS_OUTPUT.PUT_LINE('Student ID: ' || p_student_id || ' succesfully update the information on the system.');
+  EXCEPTION
+  WHEN CHECK_CONSTRAINT_VIOLATION THEN
+  DBMS_OUTPUT.PUT_LINE('Create Student failed due to check constraint violation!!!!!');
+  ROLLBACK;
+  WHEN OTHERS THEN
+  DBMS_OUTPUT.PUT_LINE('Error' || SQLERRM);
+  ROLLBACK;
+  END update_student_sp;
+  
+  
+  
+DECLARE
+    lv_student_id STUDENT.STUDENT_ID%TYPE:= 2530;
+    lv_student_first_name STUDENT.STUDENT_FIRST_NAME%TYPE := 'NANA';
+    lv_student_middle_name STUDENT.STUDENT_MIDDLE_NAME%TYPE := '';
+    lv_student_last_name STUDENT.STUDENT_LAST_NAME%TYPE := 'LULU';
+    lv_telephone STUDENT.TELEPHONE%TYPE :='8888888888888';
+    lv_email STUDENT.EMAIL%TYPE := 'NANALULU@gmail.com';
+    lv_sin STUDENT.SIN%TYPE := 9999;
+    lv_dob STUDENT.DOB%TYPE := '25-AUG-89';
+    BEGIN
+    update_student_sp(lv_student_id,lv_student_first_name,lv_student_middle_name,lv_student_last_name
+    ,lv_telephone,lv_email,lv_sin,lv_dob);
+    END;  
