94a44c3eddef8e9b24570664123c8b2bfaa6db3f
ChristyJose08 <119276072+ChristyJose08@users.noreply.github.com>
2022-12-01 11:29:37 -0500

    Update update_enrollment_sp.sql
    done some changes

--- procedures/update_enrollment_sp.sql
+++ procedures/update_enrollment_sp.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE PROCEDURE update_enrollment_sp
 ( 
   p_enrollment_id  IN ENROLLMENT.ENROLLMENT_ID%TYPE,
-  p_student_id IN ENROLLMENT.STUDENT_ID%TYPE,
+  student_id IN ENROLLMENT.STUDENT_ID%TYPE,
   p_program_id IN ENROLLMENT.PROGRAM_ID%TYPE,
   p_status IN ENROLLMENT.STATUS%TYPE,
   p_current_term IN ENROLLMENT.CURRENT_TERM%TYPE,
@@ -13,7 +13,7 @@ CREATE OR REPLACE PROCEDURE update_enrollment_sp
   PRAGMA EXCEPTION_INIT(CHECK_CONSTRAINT_VIOLATION, -2290);
 BEGIN
   UPDATE ENROLLMENT
-  SET p_student_id = p_student_id, program_id = p_program_id, status = p_status, current_term = p_current_term,
+  SET student_id = p_student_id, program_id = p_program_id, status = p_status, current_term = p_current_term,
   enroll_date = p_enrollment_date,total_fees = p_total_fees
   WHERE ENROLLMENT_ID = p_enrollment_id;
   COMMIT;
@@ -25,4 +25,4 @@ EXCEPTION
   WHEN OTHERS THEN
   DBMS_OUTPUT.PUT_LINE('Error' || SQLERRM);
   ROLLBACK;
-END update_student_sp;
+END update_enrollment_sp;
